6cd7877782bd9223afe97c9ad74bc331822b7ffe
Dexora Solutions <dinethdisanayaka11@gmail.com>
2026-02-18 09:12:33 +0530

    [bff-service] update: config & requirements

---
+++ src/bff-service/app/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "TechSalary BFF"
+
+    # URL of the internal Identity Service
+    # For Docker/K8s, this will be "http://identity-service:8001"
+    IDENTITY_SERVICE_URL: str = "http://localhost:8001/api/v1"
+
+    class Config:
+        case_sensitive = True
+
+settings = Settings()
