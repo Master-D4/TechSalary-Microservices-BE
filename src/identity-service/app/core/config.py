918bf1d6de22f81b716d96246d9ae2767f9f8e7a
Dineth Dissanayaka <dineth.d@ambrumsolutions.com>
2026-03-19 16:08:08 +0530

    update: (identity-service) DATABASE_URL in config

--- src/identity-service/app/core/config.py
+++ src/identity-service/app/core/config.py
@@ -5,7 +5,9 @@ load_dotenv()
 
 class Settings:
     PROJECT_NAME: str = "Identity Service"
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:PostgreSQL%40123@localhost:5432/techsalary_db")
+    # (LOCAL RUINING DB URL)
+    # DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:PostgreSQL%40123@localhost:5432/techsalary_db")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://myuser:mypassword@postgres-db:5432/techsalary_db")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "super_secret_key_for_dev_only")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
