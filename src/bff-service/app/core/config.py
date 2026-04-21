17ceae4e496a4f4a3db5ab01ba57c0d8aa6a4df7
supuni97 <sitharabandara1997@gmail.com>
2026-04-21 10:51:23 +0530

    updated salary service port in bff

--- src/bff-service/app/core/config.py
+++ src/bff-service/app/core/config.py
@@ -9,9 +9,7 @@ class Settings(BaseSettings):
     # For Docker/K8s, this will be "http://identity-service:8001"
     IDENTITY_SERVICE_URL: str = "http://localhost:8001/api/v1"
     SEARCH_SERVICE_URL: str = "http://localhost:8004/api/v1"
-
-    #Salary service url
-    SALARY_SERVICE_URL: str = "http://localhost:8000/api/v1/salary"
+    SALARY_SERVICE_URL: str = "http://localhost:8002/api/v1/salary"
     class Config:
         case_sensitive = True
 
